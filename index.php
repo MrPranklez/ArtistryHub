7285cd17bb922fc991632cc7da4cdf1c0ad0bf38
Petervrancken <peter.vrancken88@gmail.com>
2020-12-28 19:43:58 +0100

    Navbar complete

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Artistry Hub</title>
+    <title>ArtistryHub</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <h1>Document</h1>
+<nav class="navbar navbar-light bg-danger container d-flex">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-disc" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 4a4 4 0 0 0-4 4 .5.5 0 0 1-1 0 5 5 0 0 1 5-5 .5.5 0 0 1 0 1zm4.5 3.5a.5.5 0 0 1 .5.5 5 5 0 0 1-5 5 .5.5 0 0 1 0-1 4 4 0 0 0 4-4 .5.5 0 0 1 .5-.5z"/>
+    </svg>
+    <a class="navbar-brand text-white flex-grow-1 ml-2">ArtistryHub</a>
+    <button class="btn btn-outline-light mr-2" type="submit">HOME</button>
+    <button class="btn btn-outline-light mr-2" type="submit">MANAGEMENT</button>
+    <button class="btn btn-outline-light mr-2" type="submit">PROFILE</button>
+    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+        </svg></a>
+</nav>
 </body>
 </html>
