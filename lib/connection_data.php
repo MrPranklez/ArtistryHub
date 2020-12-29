83ee8ea61554ebcee3b0c357063bf270dc0c06f1
MrPranklez <kristof.kinnart>
2020-12-30 00:46:41 +0100

    added all php lib files + configuration

---
+++ lib/connection_data.php
@@ -0,0 +1,5 @@
+<?php
+$servername = "syntra.kinnart.cloud";
+$username = "syntra-root";
+$password = "Syntrapvkk2021!";
+$dbname = "artistryHub";
--- management.php
+++ management.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'lib/html_function.php';
+require_once 'lib/html_functions.php';
 //head printen
 PrintHead();
 ?>
