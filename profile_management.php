7175755832898296fb9fa4e200826d2affa2b5e3
Petervrancken <peter.vrancken88@gmail.com>
2021-01-23 04:07:24 +0100

    forms csrf set but not yet working

--- profile_management.php
+++ profile_management.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
 require_once 'lib/autoload.php';
 //head printen
 PrintHead();
@@ -26,10 +29,23 @@ PrintHeader();
     //                              DE SAFEBUTTON VERVANGT HIER GEGEWOON GEGEVENS
     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+
+
     //get the right data from the database
     $rows_accounts = GetData( "select * from accounts where acc_id = 1");///// tijdelijk op 1 gezet om te testen
-    $rows_projects = GetData( "select * from projects where proj_id = 1");
-    $rows_events = GetData( "select * from events where ev_id = 1");
+    ///
+    /// push the right data to the database
+    $rows_projects = [ 0 => [ "proj_name" => "", "proj_desc" => "", "proj_date" => "" ]];
+    $rows_events = [ 0 => [ "ev_loc" => "", "ev_link" => "", "ev_start_time" => "", "ev_end_time" => "", "ev_price" => "" ]];
+
+
+    $row = $rows_accounts[0];
+
+    $extra_elements['csrf_token'] = GenerateCSRF("profile_management.php" );
+    $extra_elements['select_pic'] = MakeSelect( $fkey = 'acc_id',
+        $value = $row['acc_id'] ,
+        $sql = "select acc_id, acc_prof_pic from accounts" );
+
 
     //get template
     $template_accounts = file_get_contents("templates/form_profile.html");
@@ -40,6 +56,19 @@ PrintHeader();
     $merge_accounts = MergeViewWithData($template_accounts, $rows_accounts);
     $merge_projects = MergeViewWithData($template_projects, $rows_projects);
     $merge_events = MergeViewWithData($template_events, $rows_events);
+
+    $merge_accounts = MergeViewWithExtraElements( $merge_accounts, $extra_elements );
+    $merge_projects = MergeViewWithExtraElements( $merge_projects, $extra_elements );
+    $merge_events = MergeViewWithExtraElements( $merge_events, $extra_elements );
+
+    $merge_accounts = MergeViewWithErrors( $merge_accounts, $errors );
+    $merge_projects = MergeViewWithErrors( $merge_projects, $errors );
+    $merge_events = MergeViewWithErrors( $merge_events, $errors );
+
+    $merge_accounts = RemoveEmptyErrorTags( $merge_accounts, $rows_accounts );
+    $merge_projects = RemoveEmptyErrorTags( $merge_projects, $rows_projects );
+    $merge_events = RemoveEmptyErrorTags( $merge_events, $rows_events );
+
 
     print $merge_accounts;
     print $merge_projects;
