ab45bae1593b7b259e6597902f56276c56c965e9
Petervrancken <peter.vrancken88@gmail.com>
2021-01-28 00:08:11 +0100

    op logout zonder user ook no access toegevoegd

--- logout.php
+++ logout.php
@@ -2,6 +2,10 @@
 
 require_once "lib/autoload.php";
 
+if(!key_exists('acc_id',$_SESSION['user'])){
+    GoToNoAccess();
+}
+
 unset($_SESSION['user']);
 session_destroy();
 session_regenerate_id();
